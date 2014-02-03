17a320bcf4d42b86ec4014dbccf283db27926e03
KipperOrigin <kcbsewell2@hotmail.com>
2014-02-03 11:32:30 -0800

    Add Shotgun TnT and LaunchTnT Class

--- src/kipperorigin/armamentseffects/effects/AE_EffectTnT.java
+++ src/kipperorigin/armamentseffects/effects/AE_EffectTnT.java
@@ -2,13 +2,11 @@ package kipperorigin.armamentseffects.effects;
 
 import kipperorigin.armamentseffects.AE_Main;
 import kipperorigin.armamentseffects.event.AE_InteractEvent;
+import kipperorigin.armamentseffects.resources.AE_LaunchTnT;
 import kipperorigin.armamentseffects.resources.AE_RemoveItem;
 
 import org.bukkit.Location;
-import org.bukkit.World;
 import org.bukkit.entity.Player;
-import org.bukkit.entity.TNTPrimed;
-import org.bukkit.event.block.Action;
 import org.bukkit.util.Vector;
 
 public class AE_EffectTnT extends AE_EffectParent {
@@ -21,48 +19,136 @@ public class AE_EffectTnT extends AE_EffectParent {
 	}
 
 	AE_RemoveItem AE_RI = new AE_RemoveItem();
+	AE_LaunchTnT tnt = new AE_LaunchTnT();
 
 	@Override
 	public void run(final AE_InteractEvent event) {
+		
+		/* -t Trishot
+		 * -sg Shotgun
+		 * -mr Mortar
+		 * -mn Mine
+		 * -sn Sniper?
+		 */
+		
+		String types[] = {"-t","-sg","-mr","-mn","-sn","c"};
 		final String args[] = event.getArgs();
 		Player player = event.getPlayer();
 		Location loc = player.getLocation();
-		World world = loc.getWorld();
 		int multiply = 1;
 		int timer = 0;
+		loc = loc.add(0, 1, 0);
+		
+		Vector x = new Vector(0, .35, 0);
+		Vector sg[] = new Vector[]{new Vector(Math.random(), Math.random(), Math.random()), new Vector(Math.random(), Math.random(), Math.random()), new Vector(Math.random(), Math.random(), Math.random()), new Vector(Math.random(), Math.random(), Math.random()), new Vector(Math.random(), Math.random(), Math.random()), new Vector(Math.random(), Math.random(), Math.random()), new Vector(Math.random(), Math.random(), Math.random()), new Vector(Math.random(), Math.random(), Math.random())};
 
-		if (args.length >= 1) {
-			try {
-				multiply = Integer.parseInt(args[0]);
-			} catch (NumberFormatException e) {
-				return;
-			}
-		}
-
-		if (args.length >= 2) {
-			try {
-				timer = Integer.parseInt(args[1]);
-			} catch (NumberFormatException e) {
-				return;
-			}
-		}
-
-		if (args.length >= 3) {
+		if (!(args.length >= 1))
 			return;
-		}
-
-		timer *= 20;
-
-		if ((event.getAction() == Action.RIGHT_CLICK_AIR) || (event.getAction() == Action.RIGHT_CLICK_BLOCK)) {
-			loc = loc.add(0, 1, 0);
-			TNTPrimed tnt = world.spawn(loc, TNTPrimed.class);
-			Vector x = new Vector(0, .35, 0);
+		
+		if (args[1].equalsIgnoreCase(types[0])) { 
+			if (args.length >= 2) {
+				try {
+					multiply = Integer.parseInt(args[1]);
+				} catch (NumberFormatException e) {
+					return;
+				}
+			}
+	
+			if (args.length >= 3) {
+				try {
+					timer = Integer.parseInt(args[2]);
+				} catch (NumberFormatException e) {
+					return;
+				}
+			}
+	
+			if (args.length >= 4) {
+				return;
+			}
 			Vector v = (event.getPlayer().getEyeLocation().getDirection()).multiply(multiply);
 			v.add(x);
-			tnt.setVelocity(v);
-			if (timer >= 1) {
-				tnt.setFuseTicks(timer);
+			if (timer <= 0)
+				tnt.fireTnTNoTimer(v, loc);
+			else
+				tnt.fireTnT(v, loc, timer);
+			
+			Vector a[] = new Vector[]{v, v, v, v, v, v, v, v};
+			
+			a[0].add(sg[0]);
+			if (timer <= 0)
+				tnt.fireTnTNoTimer(a[0], loc);
+			else
+				tnt.fireTnT(a[0], loc, timer);
+			
+			a[1].add(sg[1]);
+			if (timer <= 0)
+				tnt.fireTnTNoTimer(a[1], loc);
+			else
+				tnt.fireTnT(a[1], loc, timer);
+			
+			a[2].add(sg[2]);
+			if (timer <= 0)
+				tnt.fireTnTNoTimer(a[2], loc);
+			else
+				tnt.fireTnT(a[2], loc, timer);
+			
+			a[3].add(sg[3]);
+			if (timer <= 0)
+				tnt.fireTnTNoTimer(a[3], loc);
+			else
+				tnt.fireTnT(a[3], loc, timer);
+			
+			a[4].add(sg[4]);
+			if (timer <= 0)
+				tnt.fireTnTNoTimer(a[4], loc);
+			else
+				tnt.fireTnT(a[4], loc, timer);
+			
+			a[5].add(sg[5]);
+			if (timer <= 0)
+				tnt.fireTnTNoTimer(a[5], loc);
+			else
+				tnt.fireTnT(a[5], loc, timer);
+			
+			a[6].add(sg[6]);
+			if (timer <= 0)
+				tnt.fireTnTNoTimer(a[6], loc);
+			else
+				tnt.fireTnT(a[6], loc, timer);
+			
+			a[7].add(sg[7]);
+			if (timer <= 0)
+				tnt.fireTnTNoTimer(a[7], loc);
+			else
+				tnt.fireTnT(a[7], loc, timer);
+		}
+		
+		if (args[1].equalsIgnoreCase(types[5])) { 
+			if (args.length >= 2) {
+				try {
+					multiply = Integer.parseInt(args[1]);
+				} catch (NumberFormatException e) {
+					return;
+				}
 			}
+	
+			if (args.length >= 3) {
+				try {
+					timer = Integer.parseInt(args[2]);
+				} catch (NumberFormatException e) {
+					return;
+				}
+			}
+	
+			if (args.length >= 4) {
+				return;
+			}
+			Vector v = (event.getPlayer().getEyeLocation().getDirection()).multiply(multiply);
+			v.add(x);
+			if (timer <= 0)
+				tnt.fireTnTNoTimer(v, loc);
+			else
+				tnt.fireTnT(v, loc, timer);
 			AE_RI.removeItem(event.getPlayer());
 		}
 	}
---
+++ src/kipperorigin/armamentseffects/resources/AE_LaunchTnT.java
@@ -0,0 +1,19 @@
+package kipperorigin.armamentseffects.resources;
+
+import org.bukkit.Location;
+import org.bukkit.entity.TNTPrimed;
+import org.bukkit.util.Vector;
+
+public class AE_LaunchTnT {
+
+	public void fireTnT(Vector v, Location l, int i) {
+		TNTPrimed tnt = l.getWorld().spawn(l, TNTPrimed.class);
+		tnt.setVelocity(v);
+		tnt.setFuseTicks(i*20);
+	}
+	
+	public void fireTnTNoTimer(Vector v, Location l) {
+		TNTPrimed tnt = l.getWorld().spawn(l, TNTPrimed.class);
+		tnt.setVelocity(v);
+	}
+}
