93334d6acbcb098b5d6be0429ffcfb7259b85738
BuildTools <unconfigured@null.spigotmc.org>
2017-02-21 13:18:08 +0100

    Added filter parameter for effect list

--- src/main/java/org/cubeville/effects/commands/EffectListCommand.java
+++ src/main/java/org/cubeville/effects/commands/EffectListCommand.java
@@ -7,6 +7,7 @@ import java.util.Set;
 
 import org.bukkit.entity.Player;
 import org.cubeville.commons.commands.Command;
+import org.cubeville.commons.commands.CommandParameterString;
 import org.cubeville.commons.commands.CommandResponse;
 import org.cubeville.effects.managers.EffectManager;
 
@@ -14,13 +15,19 @@ public class EffectListCommand extends Command
 {
     public EffectListCommand() {
 	super("effect list");
+        addBaseParameter(new CommandParameterString()); // Filter
     }
 
     public CommandResponse execute(Player player, Set<String> flags, Map<String, Object> parameters, List<Object> baseParameters) {
-        player.sendMessage("--------------- Effect List ---------------");
+        player.sendMessage("--- Effect List ---");
         List<String> effects = EffectManager.getInstance().getEffectList();
         Collections.sort(effects);
-        for(String e: effects) player.sendMessage(e);
+        String filter = ((String) baseParameters.get(0)).toUpperCase();
+        for(String e: effects) {
+            if(filter.equals("ALL") || e.toUpperCase().indexOf(filter) >= 0) {
+                player.sendMessage(e);
+            }
+        }
         return null;
     }
 
