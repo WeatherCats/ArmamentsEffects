c4313c6c7bb3d5ad20ccf913728ad374e9b10f15
ToeDev <73237279+ToeDev@users.noreply.github.com>
2023-06-25 12:51:12 -0400

    Update SpawnFrogWithLivingEntityEffect.java

--- src/main/java/org/cubeville/effects/managers/SpawnFrogWithLivingEntityEffect.java
+++ src/main/java/org/cubeville/effects/managers/SpawnFrogWithLivingEntityEffect.java
@@ -2,6 +2,7 @@ package org.cubeville.effects.managers;
 
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
+import org.bukkit.configuration.serialization.SerializableAs;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Frog;
 import org.bukkit.entity.LivingEntity;
@@ -12,40 +13,47 @@ import org.bukkit.event.player.PlayerInteractEvent;
 import java.util.List;
 import java.util.Map;
 
+@SerializableAs("SpawnFrogWithLivingEntityEffect")
 public class SpawnFrogWithLivingEntityEffect extends EffectWithLivingEntity {
 
-    private Frog.Variant varient;
+    private Frog.Variant variant;
 
-    public SpawnFrogWithLivingEntityEffect(String name, Frog.Variant varient) {
+    public SpawnFrogWithLivingEntityEffect(String name, Frog.Variant variant) {
         setName(name);
-        this.varient = varient;
+        this.variant = variant;
     }
 
     public SpawnFrogWithLivingEntityEffect(Map<String, Object> config) {
-        String var = (String) config.get("varient");
+        String var = (String) config.get("variant");
         if(var.equalsIgnoreCase("cold")) {
-            varient = Frog.Variant.COLD;
+            variant = Frog.Variant.COLD;
         } else if(var.equalsIgnoreCase("temperate")) {
-            varient = Frog.Variant.TEMPERATE;
+            variant = Frog.Variant.TEMPERATE;
         } else if(var.equalsIgnoreCase("warm")) {
-            varient = Frog.Variant.WARM;
+            variant = Frog.Variant.WARM;
         }
         setName((String) config.get("name"));
     }
 
     public Map<String, Object> serialize() {
         Map<String, Object> ret = getSerializationBase();
-        ret.put("varient", varient.getKey());
+        if(variant.equals(Frog.Variant.COLD)) {
+            ret.put("variant", "cold");
+        } else if(variant.equals(Frog.Variant.TEMPERATE)) {
+            ret.put("variant", "temperate");
+        } else if(variant.equals(Frog.Variant.WARM)) {
+            ret.put("variant", "warm");
+        }
         return ret;
     }
 
     public void play(LivingEntity entity, Event event) {
         if(event instanceof PlayerInteractEvent) {
-            System.out.println("Spawning frog varient " + varient);
+            System.out.println("Spawning frog variant " + variant);
             if(entity instanceof Player) {
                 Location location = entity.getTargetBlock(null, 5).getLocation().add(0.5, 1, 0.5);
                 Frog frog = (Frog) entity.getWorld().spawnEntity(location, EntityType.FROG);
-                frog.setVariant(varient);
+                frog.setVariant(variant);
                 if(location.getWorld().getLivingEntities().contains(frog)) {
                     ((PlayerInteractEvent)event).getItem().setAmount(((PlayerInteractEvent)event).getItem().getAmount() - 1);
                 } else {
@@ -57,11 +65,11 @@ public class SpawnFrogWithLivingEntityEffect extends EffectWithLivingEntity {
 
     public List<String> getInfo(boolean detailed) {
         List<String> ret = getInfoBase();
-        ret.add("Varient: " + varient);
+        ret.add("Variant: " + variant);
         return ret;
     }
 
     public String getType() {
-        return "Varient";
+        return "Variant";
     }
 }
